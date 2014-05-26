1c247b89e2c96429666e45eff0759f80313a0b47
Tony McCrae <tony@eelpieconsulting.co.uk>
2014-05-26 19:54:06 +0100

    Equals method for testing.

--- src/main/java/uk/co/squadlist/web/model/Outing.java
+++ src/main/java/uk/co/squadlist/web/model/Outing.java
@@ -38,6 +38,31 @@ public class Outing {
 	}
 	
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Outing other = (Outing) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	@Override
 	public String toString() {
 		return "Outing [id=" + id + ", squad=" + squad + ", date=" + date + ", notes=" + notes + "]";
 	}
