8bbbddc999a20e8e3b8163d43307c01add7d18d2
Tony McCrae <tony@eelpieconsulting.co.uk>
2014-12-30 10:54:17 +0000

    Import.

--- src/main/java/uk/co/squadlist/web/api/SquadlistApi.java
+++ src/main/java/uk/co/squadlist/web/api/SquadlistApi.java
@@ -14,6 +14,7 @@ import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
