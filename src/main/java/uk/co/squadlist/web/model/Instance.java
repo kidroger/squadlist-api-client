62aa0a266583baca10bb9a9e20122454409be578
tony <tony@eelpieconsulting.co.uk>
2015-03-30 19:38:53 +0100

    Instance implement equals on id.

--- src/main/java/uk/co/squadlist/web/model/Instance.java
+++ src/main/java/uk/co/squadlist/web/model/Instance.java
@@ -2,31 +2,31 @@ package uk.co.squadlist.web.model;
 
 
 public class Instance {
-	
+
 	private String id;
 	private String name;
 	private String timeZone;
 	private boolean availabilityVisible;
 	private Boolean beta;
 	private String memberOrdering;
-		
+
 	public Instance() {
 	}
-	
+
 	public Instance(String id, String name, String timeZone, boolean availabilityVisible) {
 		this.id = id;
 		this.name = name;
 		this.timeZone = timeZone;
 		this.availabilityVisible = availabilityVisible;
 	}
-	
+
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
@@ -40,7 +40,7 @@ public class Instance {
 	public void setTimeZone(String timeZone) {
 		this.timeZone = timeZone;
 	}
-	
+
 	public boolean isAvailabilityVisible() {
 		return availabilityVisible;
 	}
@@ -54,12 +54,37 @@ public class Instance {
 	public void setBeta(Boolean beta) {
 		this.beta = beta;
 	}
-	
+
 	public String getMemberOrdering() {
 		return memberOrdering;
 	}
 	public void setMemberOrdering(String memberOrdering) {
 		this.memberOrdering = memberOrdering;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Instance other = (Instance) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	@Override
@@ -69,5 +94,5 @@ public class Instance {
 				+ memberOrdering + ", name=" + name + ", timeZone=" + timeZone
 				+ "]";
 	}
-	
+
 }
