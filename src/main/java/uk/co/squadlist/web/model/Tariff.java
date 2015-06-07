fb1f1babb7cb3a619ff1946f95397138ab3ead2d
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-06-07 19:19:06 +0100

    Tariff details.

--- src/main/java/uk/co/squadlist/web/model/Tariff.java
+++ src/main/java/uk/co/squadlist/web/model/Tariff.java
@@ -2,6 +2,22 @@ package uk.co.squadlist.web.model;
 
 public enum Tariff {
 
-	PRE_JUNE_2015, ONE_MONTH_FREE_TRIAL, STANDARD_THREE_MONTHS
+	PRE_JUNE_2015(null, 0), ONE_MONTH_FREE_TRIAL(1, 0), STANDARD_THREE_MONTHS(3, 36);
+
+	Integer duration;
+	int price;
+
+	private Tariff(Integer duration, int price) {
+		this.duration = duration;
+		this.price = price;
+	}
+
+	public Integer getDuration() {
+		return duration;
+	}
+
+	public int getPrice() {
+		return price;
+	}
 
 }
