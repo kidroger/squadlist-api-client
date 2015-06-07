5a09f91c7f6aaf27f36c540632b2e9cf92104491
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-06-07 15:09:33 +0100

    toString.

--- src/main/java/uk/co/squadlist/web/model/Subscription.java
+++ src/main/java/uk/co/squadlist/web/model/Subscription.java
@@ -3,30 +3,35 @@ package uk.co.squadlist.web.model;
 import java.util.Date;
 
 public class Subscription {
-	
+
 	public Subscription() {
 	}
-	
+
 	public Subscription(Tariff tariff, Date expires) {
 		this.tariff = tariff;
 		this.expires = expires;
 	}
-		
+
 	private Tariff tariff;
 	private Date expires;
-	
+
 	public Tariff getTariff() {
 		return tariff;
 	}
 	public void setTariff(Tariff tariff) {
 		this.tariff = tariff;
 	}
-	
+
 	public Date getExpires() {
 		return expires;
 	}
 	public void setExpires(Date expires) {
 		this.expires = expires;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Subscription [tariff=" + tariff + ", expires=" + expires + "]";
+	}
+
 }
